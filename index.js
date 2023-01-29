5af9ecedbb55dd6a7a30f8d7ed5ded5f4159be27
Juliane Cardoso <julianesilvac75@gmail.com>
2023-01-29 18:18:10 +0000

    feat: implementing rules and playRound functions

--- index.js
+++ index.js
@@ -1,16 +1,80 @@
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+
 function computerPlay() {
-    randomNumber = Math.floor(Math.random() * 3) + 1;
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
 
     switch (randomNumber) {
         case 1:
-            return 'Rock';
+            return ROCK;
         case 2:
-            return 'Paper';
+            return PAPER;
         case 3:
-            return 'Scissors';
+            return SCISSORS;
         default:
             break
     }
 }
 
-console.log(computerPlay());
+function rockRules(opponent) {
+    switch (opponent) {
+        case PAPER:
+            return 'You win! Paper beats Rock!';
+        case SCISSORS:
+            return 'You lost! Rock beats Scissors!';
+        case ROCK:
+            return "It's a tie!";
+        default:
+            return 'Invalid input';
+    }
+}
+
+function paperRules(opponent) {
+    switch (opponent) {
+        case PAPER:
+            return "It's a tie!";
+        case SCISSORS:
+            return 'You win! Scissors beat Paper!';
+        case ROCK:
+            return "You lost! Paper beats Rock!";
+        default:
+            return 'Invalid input';
+    }
+}
+
+function scissorsRules(opponent) {
+    switch (opponent) {
+        case PAPER:
+            return 'You lost! Scissors beat Paper!';
+        case SCISSORS:
+            return "It's a tie!";
+        case ROCK:
+            return 'You win! Rock beats Scissors!'
+        default:
+            return 'Invalid input';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    // !! use trim function in the future
+    const lowerPlayerSelection = playerSelection.toLowerCase().trim();
+
+    if (computerSelection === ROCK) {
+        return rockRules(lowerPlayerSelection);
+    } else if (computerSelection === PAPER) {
+        return paperRules(lowerPlayerSelection);
+    } else {
+        return scissorsRules(lowerPlayerSelection)
+    }
+}
+
+const playerSelection = 'SCISSORS';
+// const playerSelection = 'PAPER';
+// const playerSelection = '     paper';
+// const playerSelection = 'paper   ';
+// const playerSelection = '    paper    ';
+const computerSelection = computerPlay();
+
+console.log('computador:', computerSelection);
+console.log(playRound(playerSelection, computerSelection));
